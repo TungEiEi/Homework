074ea1a51f11af371faeb8c75fdb4dd77ef3bfd3
TungEiEi <110532849+TungEiEi@users.noreply.github.com>
2022-08-28 23:46:32 +0700

    Add files via upload

---
+++ lab2_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+   int a, b;
+   int min;
+   printf("Enter first number : ");
+   scanf("%d",&a);
+   printf("Enter second number : ");
+   scanf("%d",&b);
+   if (a<b)
+   { min=a; }
+   else
+   { min=b; }
+   for (int i = min; i>0; i--)
+   {
+    if (a%i==0 && b%i==0)
+    {
+        printf("Greatest common divisor = %d",i);
+        break;
+    }
+   }
+}
