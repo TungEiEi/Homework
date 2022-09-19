6e04ffa23d366c578c7381262b6801e428d784e3
TungEiEi <110532849+TungEiEi@users.noreply.github.com>
2022-09-19 22:00:39 +0700

    Update หรม.c

--- หรม.c
+++ หรม.c
@@ -1,19 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int num1,num2;
+int main()
+{
+    int num1, num2;
+    int x = 1, a = 2;
     printf("Enter first number : ");
     scanf("%d", &num1);
     printf("Enter second number : ");
     scanf("%d", &num2);
-    for (int i=num1+num2; i>0 ;i--)
+    while (a < num1 || a < num2)
     {
-        if(num1%i==0 && num2%i==0)
+        if (num1 % a == 0 && num2 % a == 0)
         {
-            printf("Greatest common divisor = ");
-            printf("%d", i);
-            break;
-        }    
-    }        
+            x *= a;
+            num1 /= a;
+            num2 /= a;
+        }
+        else if (num1 % a != 0 || num2 % a != 0)
+            a++;
+    }
+    printf("%d", x);
     return 0;
 }
