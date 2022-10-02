6ea8fecec9f5f3b5621f527cb64164fcd4109722
TungEiEi <110532849+TungEiEi@users.noreply.github.com>
2022-10-02 15:17:00 +0700

    Update BMI1.c

--- BMI1.c
+++ BMI1.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 
+float BMI(float h,float w)
+{
+    int BMI=1;
+    BMI=w/(h*h);
+    return BMI;
+}
+
 int main(){
-    float w;
-    float h;
-    float BMI;
+    float h,w;
     printf("Enter height (meter): ");
     scanf("%f",&h);
     printf("Enter weight (kg): ");
     scanf("%f",&w);
-    BMI=w/(h*h);
-    printf("BMI : %.2f\n",BMI);
+    printf("BMI : %.2f\n",BMI(h,w));
     if (BMI<18.5)
     {
         printf("Underweight");
